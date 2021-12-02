9d1e0854777eb123f24d17eb3adbd2a010d01ab7
atifali1307 <94118166+atifali1307@users.noreply.github.com>
2021-12-02 14:38:20 +0530

    Add files via upload

---
+++ CONTINUE.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int i;
+  clrscr();
+  for(i=1;i<=10;i++)
+  {
+    printf("\n%d",i);
+    if(i%5==0)
+      continue;
+    printf("  Hello");
+  }
+  getch();
+}
